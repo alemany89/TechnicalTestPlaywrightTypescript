6db219221799394cf3f5fcd0959cde238cb12e08
alemany89 <alemany89@hotmail.com>
2025-08-07 19:18:52 +0200

    test: add second test scenario from User Api tests feature

--- tests/api-user.spec.ts
+++ tests/api-user.spec.ts
@@ -20,4 +20,19 @@ test.describe("User API - negative tests", () => {
       expect(responseToJson).toEqual(expectedResponseFromEmptyUser);
     });
   });
+
+  test("Get a user that does not exist", async ({ userService }) => {
+    const noExistingUsername = "nonExistentUser";
+    let responseToJson: any;
+
+    await test.step("When I try to get a user that does not exist", async () => {
+      const response = await userService.getUserByUsername(noExistingUsername);
+      expect(response.status()).toBe(404);
+      responseToJson = await response.json();
+    });
+
+    await test.step("Then the message should indicate the user was not found", async () => {
+      expect(responseToJson.message).toContain(`User not found`);
+    });
+  });
 });
